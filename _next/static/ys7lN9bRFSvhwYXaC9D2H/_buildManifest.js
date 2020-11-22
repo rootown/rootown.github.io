b9ee25f0171d6ca67358a603fe92ea8c6f1ba601
themaxdavitt <max@davitt.me>
2020-11-22 07:52:53 +0000

    Deploying to master from staging @ c064e57c5f671864519326be603e4bb852e88c94 🚀

---
+++ _next/static/ys7lN9bRFSvhwYXaC9D2H/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F731d6be7a87831aff1a8.css","static\u002Fchunks\u002Fpages\u002Findex-35b0c191b55178c97cfc.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-894b8f0b2c3d46635439.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
